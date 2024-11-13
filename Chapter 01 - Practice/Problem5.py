2ace3c3b3b6f1e183d309458a157632630a5296d
Dnyaneshwar Mogal <maulimogal@gmail.com>
2024-11-13 12:12:37 +0530

    Chapter 1 Practice

---
+++ Chapter 01 - Practice/Problem5.py
@@ -0,0 +1,18 @@
+import os
+
+def print_directory_contents(path):
+    try:
+        # List all files and directories in the specified path
+        with os.scandir(path) as entries:
+            for entry in entries:
+                print(entry.name)
+    except FileNotFoundError:
+        print("The specified directory does not exist.")
+    except PermissionError:
+        print("Permission denied to access the specified directory.")
+
+# Specify the path to the directory
+directory_path = '.'  # Current directory
+print_directory_contents(directory_path)
+
+#Just added the commnent
